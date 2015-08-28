dd112f44d462e020c5fb694013337e545bf345a1
Men_in_black007 <sanyam.53jain@gmail.com>
2015-08-28 12:20:52 -0400

    a3lte: Add sound config
    Signed-off-by: Men_in_black007 <sanyam.53jain@gmail.com>

--- device_a3lte.mk
+++ device_a3lte.mk
@@ -19,6 +19,11 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, build/target/product/full.mk)
 
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
