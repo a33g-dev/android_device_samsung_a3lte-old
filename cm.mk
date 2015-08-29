eb805f91f5e7f69a9ff77e90a6b48befb2a94905
Men_in_black007 <sanyam.53jain@gmail.com>
2015-08-29 02:55:51 -0400

    a3lte: Add Build Fingerprints
    Signed-off-by: Men_in_black007 <sanyam.53jain@gmail.com>

--- cm.mk
+++ cm.mk
@@ -7,6 +7,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/a3lte/device_a3lte.mk)
 
+#Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=samsung/a33gxx/a33g:5.0.2/LRX22G/A300HXXU1BOFB:user/release-keys 
+PRIVATE_BUILD_DESC="a33gxx-user 5.0.2 LRX22G A300HXXU1BOFB release-keys"
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a3lte
 PRODUCT_NAME := cm_a3lte
