1ad7e53ab646960d6cc773ccb86006ba3608f1a0
Men_in_black007 <sanyam.53jain@gmail.com>
2015-08-29 02:35:10 -0400

    a3lte: Remove Prebuilt Kernel
    Signed-off-by: Men_in_black007 <sanyam.53jain@gmail.com>

--- device_a3lte.mk
+++ device_a3lte.mk
@@ -7,16 +7,6 @@ $(call inherit-product-if-exists, vendor/samsung/a3lte/a3lte-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/samsung/a3lte/overlay
 
-
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/samsung/a3lte/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
 $(call inherit-product, build/target/product/full.mk)
 
 # Audio configuration
